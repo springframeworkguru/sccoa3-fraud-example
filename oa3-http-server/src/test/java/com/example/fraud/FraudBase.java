b8744683b64a689334e5344b2a6c35c25c63588c
John <john@springframework.guru>
2019-03-26 16:12:19 -0400

    update inprocess

--- oa3-http-server/src/test/java/com/example/fraud/FraudBase.java
+++ oa3-http-server/src/test/java/com/example/fraud/FraudBase.java
@@ -1,15 +1,21 @@
 package com.example.fraud;
 
+import io.restassured.RestAssured;
 import org.junit.Before;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.springframework.boot.web.server.LocalServerPort;
 
 public class FraudBase {
+
+
 	@Before
 	public void setup() {
 		RestAssuredMockMvc.standaloneSetup(new FraudDetectionController(),
 				new FraudStatsController(stubbedStatsProvider()));
 	}
+
+
 
 	private StatsProvider stubbedStatsProvider() {
 		return fraudType -> {
